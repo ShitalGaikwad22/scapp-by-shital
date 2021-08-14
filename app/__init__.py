f14fe1ca814932aa884e9b2671e4e2d3977477f7
Shital Gaikwad <58787272+shitalssg123@users.noreply.github.com>
2021-08-14 22:31:54 +0530

    Add files via upload

---
+++ app/__init__.py
@@ -0,0 +1,12 @@
+from flask import Flask
+
+def create_app():
+    app = Flask(__name__, template_folder='templates')
+    app.config["SECRET_KEY"] = "dsfuibskdbvibsidbvbb"
+    app.config["MAX_CONTENT_LENGTH"] = (16 * 1024 * 1024)
+    
+    from .views import views
+    from .predict import create_model
+    app.register_blueprint(views, url_prefix='/')
+    
+    return app
